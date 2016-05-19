d5d48e6cb5599ad2d857b94cad9f3fcb12c90646
David Tinianow <david.tinianow@gmail.com>
2016-05-18 19:39:54 -0600

    minor refactoring

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -8,11 +8,9 @@ attr_reader :card_deck
   end
 
   def cards
-    File.readlines(@filename).each do |line|
+    @card_deck = File.readlines(@filename).map do |line|
       chunk_of_line = line.chomp.split(",")
-      @card_deck << Card.new(chunk_of_line[0], chunk_of_line[1], chunk_of_line[2])
+      Card.new(chunk_of_line[0], chunk_of_line[1], chunk_of_line[2])
     end
-    @card_deck
   end
-
 end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -11,21 +11,21 @@ class CardGeneratorTest < Minitest::Test
 
   def test_card_generator_returns_array
     #skip
-    filename = "./lib/cards.txt"
+    filename = "cards.txt"
     cards = CardGenerator.new(filename).cards
     assert_instance_of Array, cards
   end
 
   def test_card_generator_array_holds_cards
     #skip
-    filename = "./lib/cards.txt"
+    filename = "cards.txt"
     cards = CardGenerator.new(filename).cards
     assert_instance_of Card, cards.first
   end
 
   def test_card_generator_contains_question_and_answer
     #skip
-    filename = "./lib/cards.txt"
+    filename = "cards.txt"
     cards = CardGenerator.new(filename).cards
     assert_equal "What is 5 + 5?", cards.first.question
     assert_equal "10", cards.first.answer
@@ -34,7 +34,7 @@ class CardGeneratorTest < Minitest::Test
 
   def test_card_generator_contains_more_than_one_question_and_answer
     #skip
-    filename = "./lib/cards.txt"
+    filename = "cards.txt"
     cards = CardGenerator.new(filename).cards
     assert_equal "What cardboard cutout lives at Turing?", cards.last.question
     assert_equal "Justin bieber", cards.last.answer
